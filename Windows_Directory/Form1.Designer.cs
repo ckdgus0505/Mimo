9022250f8f13c2258e7891bf0108c16a7e5980e7
ckdgus0505 <ckdgus0505@gmail.com>
2018-12-29 03:55:24 +0900

    make listbox function

--- Windows_Directory/Form1.Designer.cs
+++ Windows_Directory/Form1.Designer.cs
@@ -28,20 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mimLists = new System.Windows.Forms.ListBox();
+            this.showMemo = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // mimLists
+            // 
+            this.mimLists.FormattingEnabled = true;
+            this.mimLists.ItemHeight = 25;
+            this.mimLists.Location = new System.Drawing.Point(40, 43);
+            this.mimLists.Name = "mimLists";
+            this.mimLists.Size = new System.Drawing.Size(225, 479);
+            this.mimLists.TabIndex = 0;
+            this.mimLists.SelectedIndexChanged += new System.EventHandler(this.mimLists_SelectedIndexChanged);
+            // 
+            // showMemo
+            // 
+            this.showMemo.Location = new System.Drawing.Point(284, 43);
+            this.showMemo.Name = "showMemo";
+            this.showMemo.Size = new System.Drawing.Size(941, 31);
+            this.showMemo.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(44, 568);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(221, 105);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "OPEN";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(524, 568);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(221, 105);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(1004, 568);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(221, 105);
+            this.btnSync.TabIndex = 2;
+            this.btnSync.Text = "동기화";
+            this.btnSync.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(764, 568);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(221, 105);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(284, 568);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(221, 105);
+            this.btnNew.TabIndex = 2;
+            this.btnNew.Text = "NEW";
+            this.btnNew.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1271, 713);
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.showMemo);
+            this.Controls.Add(this.mimLists);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Mimo";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox mimLists;
+        private System.Windows.Forms.TextBox showMemo;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnNew;
     }
 }
 
--- Windows_Directory/Form1.cs
+++ Windows_Directory/Form1.cs
@@ -5,16 +5,94 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Mimo
 {
+
     public partial class Form1 : Form
     {
+        string dirPath = @"../mimms";
+        string filename;
+        bool hasfile;
+
         public Form1()
         {
             InitializeComponent();
+            btnSync.Click += BtnSync_Click;
+        }
+
+        private void BtnSync_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+                dirPath = fbd.SelectedPath;
+
+            if (System.IO.Directory.Exists(dirPath))
+            {
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(dirPath);
+                foreach (var item in di.GetFiles())
+                {
+                    mimLists.Items.Add(item.Name);
+                }
+            }
+
+
+            /*
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+             
+                filepath = openFileDialog1.FileName;
+                filename = filepath.Split('\\')[filepath.Split('\\').Length - 1];
+                StreamReader sr = new StreamReader(filepath);
+                string str = "";
+                while ((str = sr.ReadLine()) != null)
+                {
+                    mimLists.Items.Add(str);
+                }
+                sr.Close();
+
+                this.hasfile = true;
+                this.Text = filename;
+            }
+            */
+        }
+
+        private void FileRead(params string[] UserInfo)
+        {
+            FileStream fs = new FileStream(@"../mimms/hello.mimm", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader st = new StreamReader(fs, System.Text.Encoding.UTF8);
+
+            st.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            while (st.Peek() > -1)
+            {
+                string temp = st.ReadLine();
+                //MessageBox.Show(aaa);
+            }
+            st.Close();
+            fs.Close();
+        }
+        private void FileWrite(string str)
+        {
+            FileStream fs = new FileStream(@"../mimms/hello.mimm", FileMode.Append, FileAccess.Write);
+            //FileMode중 append는 이어쓰기. 파일이 없으면 만든다.
+
+            StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
+            sw.WriteLine(str);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        private void mimLists_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
