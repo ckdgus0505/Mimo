556fff460b6120cabc7ff5a1c0f9135437db0508
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-29 22:07:43 +0900

    Add Memo List Activity

---
+++ Android_Directory/Mimo/app/src/main/java/alpha/mimo/MemoListActivity.java
@@ -0,0 +1,13 @@
+package alpha.mimo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_list);
+    }
+}
