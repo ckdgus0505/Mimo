decbf09b6d6c6a745b47b0640cd1ce438b9d9bc9
dongseoki <43495319+dongseoki@users.noreply.github.com>
2018-12-30 14:03:21 +0900

    setOnItemclicklistener... and find files by getfilesdir
    good... haha...

--- Android_Directory/Mimo/app/src/main/java/alpha/mimo/MemoListActivity.java
+++ Android_Directory/Mimo/app/src/main/java/alpha/mimo/MemoListActivity.java
@@ -1,18 +1,24 @@
 package alpha.mimo;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MemoListActivity extends AppCompatActivity {
@@ -26,13 +32,36 @@ public class MemoListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_list);
 
-        items = new ArrayList<String>();
+        try{
+            items = new ArrayList<String>();
 
-        adapter = new ArrayAdapter<String>(MemoListActivity.this, android.R.layout.simple_list_item_single_choice, items);
+            adapter = new ArrayAdapter<String>(MemoListActivity.this, android.R.layout.simple_list_item_single_choice, items);
 
-        memoListView = (ListView) findViewById(R.id.MemoListView);
-        memoListView.setAdapter(adapter);
-        memoListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+            memoListView = (ListView) findViewById(R.id.MemoListView);
+            memoListView.setAdapter(adapter);
+            memoListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+            File directory2 = getFilesDir().getParentFile();
+
+            File[] files2 = directory2.listFiles();
+            Log.d("Files2", "Size: "+ files2.length);
+            for (int i = 0; i < files2.length; i++)
+            {
+                String str = files2[i].getName();
+                if(str.contains(".mimm"))
+                    items.add(files2[i].getName());
+                adapter.notifyDataSetChanged();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        memoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),items.get(position).toString(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void onButtonNew(View view)
