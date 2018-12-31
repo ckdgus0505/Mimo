4a12df60677020fc7168959e2adba8431e27ea4a
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-31 21:51:21 +0900

    Mod UI
    Windows 버전과 똑같이 구현했음

--- Ubuntu_Directory/Mimo.py
+++ Ubuntu_Directory/Mimo.py
@@ -3,44 +3,53 @@ from tkinter import *
 root = Tk()
 
 root.title("Mimo")
+root.resizable(False, False)
 
 
 # ==== Frame List ====
 buttonFrm = Frame(root)
-buttonFrm.grid(row=1, column=1)
+buttonFrm.grid(row=1, column=1, columnspan=2)
+
+listFrm = Frame(root, relief="solid", bd=2)
+listFrm.grid(row=2, column=1)
 
 mainFrm = Frame(root)
-mainFrm.grid(row=2, column=1)
+mainFrm.grid(row=2, column=2)
 
 
 # ==== Button Frame ====
-openBtn = Button(buttonFrm, text="Open")
+openBtn = Button(buttonFrm, width=16, text="Open")
 openBtn.grid(row=1, column=1)
 
-newBtn = Button(buttonFrm, text="New")
+newBtn = Button(buttonFrm, width=16, text="New")
 newBtn.grid(row=1, column=2)
 
-saveBtn = Button(buttonFrm, text="Save")
+saveBtn = Button(buttonFrm, width=16, text="Save")
 saveBtn.grid(row=1, column=3)
 
-deleteBtn = Button(buttonFrm, text="Delete")
+deleteBtn = Button(buttonFrm, width=16, text="Delete")
 deleteBtn.grid(row=1, column=4)
 
-convertBtn = Button(buttonFrm, text="Convert")
+convertBtn = Button(buttonFrm, width=16, text="Convert")
 convertBtn.grid(row=1, column=5)
 
-syncBtn = Button(buttonFrm, text="Sync")
+syncBtn = Button(buttonFrm, width=16, text="Sync")
 syncBtn.grid(row=1, column=6)
 
 
+# ==== List Frame ====
+memoListLsb = Listbox(listFrm, selectmod="single")
+memoListLsb.insert(0, "Test1")
+memoListLsb.insert(1, "Test2")
+memoListLsb.grid(row=1, column=1)
+
+
 # ==== Main Frame ====
-memoListLbl = Label(mainFrm, text="test\ntest")
-memoListLbl.grid(row=1, column=1)
+memoTitleEty = Entry(mainFrm, width=80)
+memoTitleEty.grid(row=1, column=1)
 
-memoTitleEty = Entry(mainFrm)
-memoTitleEty.grid(row=1, column=2)
+memoTxt = Text(mainFrm)
+memoTxt.grid(row=2, column=1)
 
-memoEty = Entry(mainFrm)
-memoEty.grid(row=2, column=2)
 
 root.mainloop()
