2f7d0dceeeeebf98b63b24fba8f1ec4c732978d4
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-31 22:02:48 +0900

    Add Button Command(Function)

--- Ubuntu_Directory/Mimo.py
+++ Ubuntu_Directory/Mimo.py
@@ -2,6 +2,32 @@ from tkinter import *
 
 root = Tk()
 
+
+# ==== Button Function ====
+def on_open_btn():
+    print("open button")
+
+
+def on_new_btn():
+    print("new button")
+
+
+def on_save_btn():
+    print("save button")
+
+
+def on_delete_btn():
+    print("delete button")
+
+
+def on_convert_btn():
+    print("convert button")
+
+
+def on_sync_btn():
+    print("sync button")
+
+
 root.title("Mimo")
 root.resizable(False, False)
 
@@ -18,22 +44,22 @@ mainFrm.grid(row=2, column=2)
 
 
 # ==== Button Frame ====
-openBtn = Button(buttonFrm, width=16, text="Open")
+openBtn = Button(buttonFrm, width=16, text="Open", command=on_open_btn)
 openBtn.grid(row=1, column=1)
 
-newBtn = Button(buttonFrm, width=16, text="New")
+newBtn = Button(buttonFrm, width=16, text="New", command=on_new_btn)
 newBtn.grid(row=1, column=2)
 
-saveBtn = Button(buttonFrm, width=16, text="Save")
+saveBtn = Button(buttonFrm, width=16, text="Save", command=on_save_btn)
 saveBtn.grid(row=1, column=3)
 
-deleteBtn = Button(buttonFrm, width=16, text="Delete")
+deleteBtn = Button(buttonFrm, width=16, text="Delete", command=on_delete_btn)
 deleteBtn.grid(row=1, column=4)
 
-convertBtn = Button(buttonFrm, width=16, text="Convert")
+convertBtn = Button(buttonFrm, width=16, text="Convert", command=on_convert_btn)
 convertBtn.grid(row=1, column=5)
 
-syncBtn = Button(buttonFrm, width=16, text="Sync")
+syncBtn = Button(buttonFrm, width=16, text="Sync", command=on_sync_btn)
 syncBtn.grid(row=1, column=6)
 
 
