a97f15f9965ccb2fc5800db7fe3c6b7925bfc15c
Lim Hyun (임현) <hyunzion@gmail.com>
2019-01-02 01:15:54 +0900

    Add Main Class
    클래스화 시켰음

--- Ubuntu_Directory/Mimo.py
+++ Ubuntu_Directory/Mimo.py
@@ -1,82 +1,77 @@
 from tkinter import *
 
-root = Tk()
+
+class Main:
+    def __init__(self):
+        root = Tk()
+
+        # ==== Button Function ====
+        def on_open_btn():
+            print("open button")
+
+        def on_new_btn():
+            print("new button")
+
+        def on_save_btn():
+            print("save button")
+
+        def on_delete_btn():
+            print("delete button")
+
+        def on_convert_btn():
+            print("convert button")
+
+        def on_sync_btn():
+            print("sync button")
+
+        root.title("Mimo")
+        root.resizable(False, False)
+        root.iconbitmap('..\images\icon.ico')
+
+        # ==== Frame List ====
+        button_frm = Frame(root)
+        button_frm.grid(row=1, column=1, columnspan=2)
+
+        list_frm = Frame(root, relief="solid", bd=2)
+        list_frm.grid(row=2, column=1)
+
+        main_frm = Frame(root)
+        main_frm.grid(row=2, column=2)
+
+        # ==== Button Frame ====
+        open_btn = Button(button_frm, width=16, text="Open", command=on_open_btn)
+        open_btn.grid(row=1, column=1)
+
+        new_btn = Button(button_frm, width=16, text="New", command=on_new_btn)
+        new_btn.grid(row=1, column=2)
+
+        save_btn = Button(button_frm, width=16, text="Save", command=on_save_btn)
+        save_btn.grid(row=1, column=3)
+
+        delete_btn = Button(button_frm, width=16, text="Delete", command=on_delete_btn)
+        delete_btn.grid(row=1, column=4)
+
+        convert_btn = Button(button_frm, width=16, text="Convert", command=on_convert_btn)
+        convert_btn.grid(row=1, column=5)
+
+        sync_btn = Button(button_frm, width=16, text="Sync", command=on_sync_btn)
+        sync_btn.grid(row=1, column=6)
+
+        # ==== List Frame ====
+        memo_list_lsb = Listbox(list_frm, selectmod="single")
+        memo_list_lsb.insert(0, "Test1")
+        memo_list_lsb.insert(1, "Test2")
+        memo_list_lsb.grid(row=1, column=1)
+
+        # ==== Main Frame ====
+        memo_title_ety = Entry(main_frm, width=80)
+        memo_title_ety.grid(row=1, column=1)
+
+        memo_txt = Text(main_frm)
+        memo_txt.grid(row=2, column=1)
+
+        root.mainloop()
 
 
-# ==== Button Function ====
-def on_open_btn():
-    print("open button")
-
-
-def on_new_btn():
-    print("new button")
-
-
-def on_save_btn():
-    print("save button")
-
-
-def on_delete_btn():
-    print("delete button")
-
-
-def on_convert_btn():
-    print("convert button")
-
-
-def on_sync_btn():
-    print("sync button")
-
-
-root.title("Mimo")
-root.resizable(False, False)
-root.iconbitmap('..\images\icon.ico')
-
-
-# ==== Frame List ====
-buttonFrm = Frame(root)
-buttonFrm.grid(row=1, column=1, columnspan=2)
-
-listFrm = Frame(root, relief="solid", bd=2)
-listFrm.grid(row=2, column=1)
-
-mainFrm = Frame(root)
-mainFrm.grid(row=2, column=2)
-
-
-# ==== Button Frame ====
-openBtn = Button(buttonFrm, width=16, text="Open", command=on_open_btn)
-openBtn.grid(row=1, column=1)
-
-newBtn = Button(buttonFrm, width=16, text="New", command=on_new_btn)
-newBtn.grid(row=1, column=2)
-
-saveBtn = Button(buttonFrm, width=16, text="Save", command=on_save_btn)
-saveBtn.grid(row=1, column=3)
-
-deleteBtn = Button(buttonFrm, width=16, text="Delete", command=on_delete_btn)
-deleteBtn.grid(row=1, column=4)
-
-convertBtn = Button(buttonFrm, width=16, text="Convert", command=on_convert_btn)
-convertBtn.grid(row=1, column=5)
-
-syncBtn = Button(buttonFrm, width=16, text="Sync", command=on_sync_btn)
-syncBtn.grid(row=1, column=6)
-
-
-# ==== List Frame ====
-memoListLsb = Listbox(listFrm, selectmod="single")
-memoListLsb.insert(0, "Test1")
-memoListLsb.insert(1, "Test2")
-memoListLsb.grid(row=1, column=1)
-
-
-# ==== Main Frame ====
-memoTitleEty = Entry(mainFrm, width=80)
-memoTitleEty.grid(row=1, column=1)
-
-memoTxt = Text(mainFrm)
-memoTxt.grid(row=2, column=1)
-
-
-root.mainloop()
+if __name__ == "__main__":
+    Main()
