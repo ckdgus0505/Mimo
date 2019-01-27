091d579dba8fdfe3f5c34c2d7c03a026ef17906c
Lim Hyun <hyunzion@gmail.com>
2019-01-27 23:31:34 +0900

    Create signup.php #9

---
+++ html/signup.php
@@ -0,0 +1,32 @@
+<?php
+	$db_host = "localhost";
+	$db_user = "root";
+	$db_passwd = "mimo";
+	$db_name = "MIMO";
+	$conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
+
+	if (mysql_connect_errno($conn))
+	{
+		echo "데이터 베이스 연결 실패" . mysql_connect_error();
+	}
+	else
+	{
+		echo "데이터 베이스 연결 성공";
+	}
+
+	echo "<br>";
+
+	$sql = "INSERT INTO MEMBER (ID, PASSWORD) VALUES ('$_POST[name]', '$_POST[password]')";
+
+	if (mysql_query($conn, $sql))
+	{
+		echo "등록 완료";
+	}
+	else
+	{
+		echo "Error: " . $sql. "<br>" . mysqli_error($conn);
+	}
+
+	mysqli_close($conn);
+
+?>
