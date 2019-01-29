8739dddbd5d1f7b5034f55d96650bd670a33994d
Lim Hyun <hyunzion@gmail.com>
2019-01-30 01:02:24 +0900

    Add Function

--- Ubuntu_Directory/Login.py
+++ Ubuntu_Directory/Login.py
@@ -1,9 +1,19 @@
 from tkinter import *
+import webbrowser
+import Mimo     # Mimo.py
 
 
 class Login:
     def __init__(self):
         root = Tk()
+
+        def on_login_btn():
+            print("login")
+            root.destroy()
+            Mimo.Main()
+
+        def on_signup_btn():
+            webbrowser.open('http://mimm.dlinkddns.com/signup.html')
 
         root.title("Mimo")
         root.resizable(False, False)
@@ -25,10 +35,10 @@ class Login:
         pw_ety = Entry(login_frm, text="Password")
         pw_ety.grid(row=2, column=2)
 
-        login_btn = Button(button_frm, text="LogIn")
+        login_btn = Button(button_frm, text="LogIn", command=on_login_btn)
         login_btn.grid(row=1, column=1)
 
-        signup_btn = Button(button_frm, text="SignUp")
+        signup_btn = Button(button_frm, text="SignUp", command=on_signup_btn)
         signup_btn.grid(row=1, column=2)
 
         root.mainloop()
